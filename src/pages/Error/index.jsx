d05ce1f5ff15377a458ad4a88926e1ce81b9f9f5
sbeou13 <design@walf.com.br>
2023-03-19 21:08:33 -0300

    Add mobile vertion

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom"
+
 function Error() {
-    return(<p>Oups! La page que vous demandez n'existe pas.</p>)
+    return(
+        <main>
+            <section className="error404">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <Link to={'./'}>Retourner sur la page d’accueil</Link>
+            </section>
+        </main>
+    )
 }
 
 export default Error
